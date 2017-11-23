e9b9ea41aded46e05da9b7a6d3ab2a8e7bd8afb1
Spencer Bryson <spencerbryson@Spencers-MacBook-Pro.local>
2017-11-23 01:45:00 -0500

    barebones physics

--- Mesh.cpp
+++ Mesh.cpp
@@ -20,6 +20,13 @@ void Mesh::generateMesh() {
             vertex v = {x, y, z, 1.f, 1.f, 1.f};
             vertices[j + (i*N)] = v;
             times[j + (i*N)] = 0;
+
+            std::vector<float> velocity;
+            velocity.resize(3);
+            velocity.push_back(0);
+            velocity.push_back(0);
+            velocity.push_back(0);
+            velocities.push_back(velocity);
             x += d;
         }
         y -= d;
@@ -49,26 +56,25 @@ void Mesh::generateIndices() {
     }
 }
 
-void Mesh::step(vector<float> pos, vector<float> dposdt, const double /* t */) {
-        float force = mass * gravity;
-        dposdt[1] = dposdt[1] - force;
-        pos[1] += dposdt[1]
-
-        return;
-    }
-
 void Mesh::update() {
     // physics here, must complete before rendering
     for (int i = 0; i < nVertices; i++) {
         vertex* curVertex = &vertices[i];
-        vector<float> position(3);
+        std::vector<float> position;
+        std::vector<float> velocity = velocities[i];
+        float t = times[i];
+
+        position.resize(3);
         position[0] = curVertex->x;
         position[1] = curVertex->y;
         position[2] = curVertex->z;
-        vector<float> velocity_ = velocity[i];
-        float t = times[i];
-        std::cout << "t at vertex one: " << t << std::endl;
-        stepper.do_step(step, position, velocity_, t, dt);
+
+        sys s;
+        stepper.do_step(s, position, t, dt);
+
+        curVertex->x = position[0];
+        curVertex->y = position[1];
+        curVertex->z = position[2];
     }
 
     return;
--- Mesh.h
+++ Mesh.h
@@ -9,6 +9,7 @@ using namespace boost::numeric::odeint;
 using namespace boost::numeric::ublas;
 
 typedef runge_kutta_dopri5<std::vector<float> > stepperType;
+typedef std::vector<float> vec;
 
 class Mesh {
 public:
@@ -28,14 +29,19 @@ public:
 
     // physics 
 
-    std::vector<vector<float> > velocity;
+    std::vector<std::vector<float> > velocities;
     const float dt = 0.01f;
     float mass = 0.01f;
     float gravity = 9.8f;
     float* times;
     stepperType stepper;
-    static void step(std::vector<float> pos, vector<float> dposdt, const double /* t */) ;
 
+    struct sys {
+        void operator()(const vec &pos, vec &velocity, double /*t*/) {
+            // need to implement other forces
+            velocity[1] = -0.98f;
+        }
+    };
     void update();
 };
 
